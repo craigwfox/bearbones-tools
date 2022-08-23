cb7a3bb6dbd20d4d5a73b9d8eaab602eee0f188f
Craig Fox <craig@craigwfox.com>
2022-08-22 21:30:39 -0500

    renamed color picker

---
+++ src/lib/colorConverter.svelte
@@ -0,0 +1,13 @@
+<fieldset id="colorConverter" aria-labelledby="title-color">
+	<legend id="title-color">Color converter</legend>
+	<div class="input-group">
+		<div class="input">
+			<label for="hex">Hex</label>
+			<input type="text" id="hex" />
+		</div>
+		<div class="input">
+			<label for="hsl">HSL</label>
+			<input type="text" id="hsl" />
+		</div>
+	</div>
+</fieldset>
