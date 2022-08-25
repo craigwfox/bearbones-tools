442a36b62534bc500c00d9c9ab019484fc55f1d0
Craig Fox <cwfox@arkbluecross.com>
2022-08-25 09:22:31 -0500

    swapped back to auto adaptor

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,11 @@
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// default options are shown
-	adapter: adapter({
-		// if true, will create a Netlify Edge Function rather
-		// than using standard Node-based functions
-		edge: false,
-
-		// if true, will split your app into multiple functions
-		// instead of creating a single one for the entire app.
-		// if `edge` is true, this option cannot be used
-		split: false
-	}),
+	kit: {
+		adapter: adapter()
+	},
 	preprocess: preprocess({})
 };
 
