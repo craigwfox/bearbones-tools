951926631a043a439cace5969b7a3ae1f6f94a23
Craig Fox <craig@craigwfox.com>
2022-08-27 18:00:24 -0500

    Tauri build setup

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,12 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			fallback: 'index.html'
+		})
 	},
 	preprocess: preprocess({})
 };
